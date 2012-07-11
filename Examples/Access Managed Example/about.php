6b1d522c6f093d8685be5b92b5c50885de749e69
wishingforayer@gmail.com <wishingforayer@gmail.com@2571ccf1-c446-ad3f-9dae-1019377a0179>
2012-07-11 18:59:21 +0000

    Testing managed access.
    git-svn-id: https://php-bouncer.googlecode.com/svn/trunk@40 2571ccf1-c446-ad3f-9dae-1019377a0179

---
+++ Examples/Access Managed Example/about.php
@@ -0,0 +1,22 @@
+<?php
+	/**
+	 * Created with JetBrains PhpStorm.
+	 * User: Brendon Dugan <wishingforayer@gmail.com>
+	 * Date: 7/11/12
+	 * Time: 2:56 PM
+	 *
+	 */
+	include "core.php";
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>About Page!</title>
+</head>
+<body>
+<h1>Welcome to about.php!</h1>
+
+<p>The user you are operating as must have "Public" access!</p>
+</body>
+</html>
---
+++ Examples/Access Managed Example/myaccount.php
@@ -0,0 +1,22 @@
+<?php
+	/**
+	 * Created with JetBrains PhpStorm.
+	 * User: Brendon Dugan <wishingforayer@gmail.com>
+	 * Date: 7/11/12
+	 * Time: 2:58 PM
+	 *
+	 */
+	include "core.php";
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Account Page!</title>
+</head>
+<body>
+<h1>Welcome to myaccount.php!</h1>
+
+<p>The user you are operating as must have "Registered User" access!</p>
+</body>
+</html>
